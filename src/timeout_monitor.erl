3293e000fa87a6ac7e2061f32af216d7eb49b954
James <icson.zhou@gmail.com>
2017-04-08 13:37:04 +0800

    增加只发送一次超时消息的方法

--- src/timeout_monitor.erl
+++ src/timeout_monitor.erl
@@ -11,7 +11,7 @@
 -author("james").
 
 %% API
--export([start_monitor/3, stop_monitor/1]).
+-export([start_monitor/3, start_once_monitor/3, stop_monitor/1]).
 
 -define(SUP_SERVER, timeout_monitor_sup).
 
@@ -19,7 +19,7 @@
 %%%===================================================================
 %%% @doc
 %%% 启动 monitor server
-%%% 仅在异常退出时supervisor会重启该server，当被监控的进程不存在时监控进程需结束
+%%% 仅在异常退出时supervisor会重启该server，
 %%%
 %%% Pid : 接收超时消息的进程
 %%% MonitorType ： 标识该超时消息类型，同一进程的标识符应该唯一
@@ -35,6 +35,23 @@ start_monitor(Pid, MonitorType, Interval) ->
 
 %%%===================================================================
 %%% @doc
+%%% 启动 monitor server，仅发送一次超时消息，自我结束
+%%%
+%%% Pid : 接收超时消息的进程
+%%% MonitorType ： 标识该超时消息类型，同一进程的标识符应该唯一
+%%% Interval : 超时间隔，ms
+%%% @end
+%%%===================================================================
+
+-spec(start_once_monitor(Pid :: pid(), MonitorType :: atom(),
+    Interval :: integer()) ->
+  {ok, ChildPid :: pid()}).
+start_once_monitor(Pid, MonitorType, Interval) ->
+  {ok, P} = supervisor:start_child(?SUP_SERVER, [Pid, MonitorType, Interval]),
+  timer:apply_after(Interval + 100, ?MODULE, stop_monitor, [P]).
+
+%%%===================================================================
+%%% @doc
 %%% 停止 monitor server
 %%% 应该仅有被监控进程发起停止监控进程的请求
 %%%
@@ -46,3 +63,5 @@ start_monitor(Pid, MonitorType, Interval) ->
   ok | {error, Error :: term()}).
 stop_monitor(Pid) ->
   supervisor:terminate_child(?SUP_SERVER, Pid).
+
+
